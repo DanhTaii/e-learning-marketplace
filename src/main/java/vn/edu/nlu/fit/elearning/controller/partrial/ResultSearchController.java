113b81e8f5425e99572b38d30535745fa68481ee
Nguyen Ngoc Minh <23130192@st.hcmuaf.edu.vn>
2025-12-11 00:11:20 +0700

    result-search-title

--- src/main/java/vn/edu/nlu/fit/elearning/controller/partrial/ResultSearchController.java
+++ src/main/java/vn/edu/nlu/fit/elearning/controller/partrial/ResultSearchController.java
@@ -3,14 +3,21 @@ package vn.edu.nlu.fit.elearning.controller.partrial;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
+import vn.edu.nlu.fit.elearning.model.Category;
+import vn.edu.nlu.fit.elearning.services.CategoryService;
 
 import java.io.IOException;
+import java.util.List;
 
-@WebServlet(name = "ResultSearchController", value = "/ResultSearchController")
+@WebServlet(name = "ResultSearchController", value = "/result-search")
 public class ResultSearchController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        int id = Integer.parseInt(request.getParameter("id"));
+        CategoryService cs = new CategoryService();
+        Category cate = cs.getCategoryById(id);
+        request.setAttribute("cate", cate);
+        request.getRequestDispatcher("/html-partrial/result-search.jsp").forward(request, response);
     }
 
     @Override
