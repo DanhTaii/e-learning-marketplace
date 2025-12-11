adcbb6c756664db09c8fd5b8d0209905510a52b5
DanhTai <23130281@st.hcmuaf.edu.vn>
2025-12-11 13:22:56 +0700

    add data to dashboard, restyle css

--- src/main/java/vn/edu/nlu/fit/elearning/controller/admin/DashboardController.java
+++ src/main/java/vn/edu/nlu/fit/elearning/controller/admin/DashboardController.java
@@ -3,13 +3,41 @@ package vn.edu.nlu.fit.elearning.controller.admin;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
+import vn.edu.nlu.fit.elearning.model.Course;
+import vn.edu.nlu.fit.elearning.model.Order;
+import vn.edu.nlu.fit.elearning.model.User;
+import vn.edu.nlu.fit.elearning.services.CourseService;
+import vn.edu.nlu.fit.elearning.services.OrderService;
+import vn.edu.nlu.fit.elearning.services.UserService;
 
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(name = "DashboardController", value = "/admin/dashboard")
 public class DashboardController extends HttpServlet {
+    private UserService userService;
+    private OrderService orderService;
+    private CourseService courseService;
+
+    public DashboardController() {
+        this.userService = new UserService();
+        this.orderService = new OrderService();
+        this.courseService = new CourseService();
+    }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Order> orderTotal = orderService.getAllOrders();
+        List<User> userTotal = userService.getAllUsers();
+        List<Course> courseTotal = courseService.getAllCourses();
+        double revenueSum = orderService.getRevenueTotal();
+        List<Course> popularCourses = courseService.getSixCoursesMostPopular();
+
+        request.setAttribute("userCount", userTotal.size());
+        request.setAttribute("orderCount", orderTotal.size());
+        request.setAttribute("courseCount", courseTotal.size());
+        request.setAttribute("revenueTotal", revenueSum);
+        request.setAttribute("popularCourses", popularCourses);
 
         request.getRequestDispatcher("/html-admin/dashboard.jsp").forward(request, response);
     }
