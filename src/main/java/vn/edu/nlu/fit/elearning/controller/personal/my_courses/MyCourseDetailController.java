f5c3285eac99e21d8a4448ea4b4af3bb3d3cf3ca
DanhTai <23130281@st.hcmuaf.edu.vn>
2026-01-05 22:01:09 +0700

    add basic information, create dto for enrollment detail, get list reviews

--- src/main/java/vn/edu/nlu/fit/elearning/controller/personal/my_courses/MyCourseDetailController.java
+++ src/main/java/vn/edu/nlu/fit/elearning/controller/personal/my_courses/MyCourseDetailController.java
@@ -3,9 +3,12 @@ package vn.edu.nlu.fit.elearning.controller.personal.my_courses;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
+import vn.edu.nlu.fit.elearning.dto.EnrollmentDetailDto;
 import vn.edu.nlu.fit.elearning.dto.LessonProgressDTO;
+import vn.edu.nlu.fit.elearning.dto.ReviewDto;
 import vn.edu.nlu.fit.elearning.model.User;
 import vn.edu.nlu.fit.elearning.services.EnrollmentService;
+import vn.edu.nlu.fit.elearning.services.ReviewService;
 import vn.edu.nlu.fit.elearning.services.UserLessonProgressService;
 
 import java.io.IOException;
@@ -15,12 +18,14 @@ import java.util.List;
 public class MyCourseDetailController extends HttpServlet {
     private EnrollmentService enrollmentService;
     private UserLessonProgressService ulp;
+    private ReviewService reviewService;
 
     @Override
     public void init() throws ServletException {
         super.init();
         this.enrollmentService = new EnrollmentService();
         this.ulp = new UserLessonProgressService();
+        this.reviewService = new ReviewService();
     }
 
     @Override
@@ -28,11 +33,16 @@ public class MyCourseDetailController extends HttpServlet {
         User user = (User) request.getSession().getAttribute("userSession");
         int userId = user.getId();
         int courseId = Integer.parseInt(request.getParameter("courseId"));
-//        System.out.println(userId);
-//        System.out.println(courseId);
+
+        List<ReviewDto> reviewDtos = reviewService.getReviewsByCourseId(courseId);
+        request.setAttribute("reviewDtos",reviewDtos);
+        EnrollmentDetailDto enrollmentDetail = enrollmentService.getEnrollmentDetail(userId);
+        enrollmentDetail.setListReviews(reviewDtos);
+
         List<LessonProgressDTO> listLessons = ulp.getAllUserLessonProgresss(userId, courseId);
-//        System.out.println(listLessons);
-        request.setAttribute("listLessons", listLessons);
+        enrollmentDetail.setListLesson(listLessons);
+
+        request.setAttribute("enrollmentDetail", enrollmentDetail);
         request.getRequestDispatcher("/html-personal/course-content.jsp").forward(request, response);
     }
 
