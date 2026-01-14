7ae3b1a0f8e2017ece40ab34be226baad093c223
DanhTai <23130281@st.hcmuaf.edu.vn>
2026-01-14 13:30:33 +0700

    add update tag, category in course detail admin page

--- src/main/java/vn/edu/nlu/fit/elearning/dao/CourseTagDao.java
+++ src/main/java/vn/edu/nlu/fit/elearning/dao/CourseTagDao.java
@@ -5,21 +5,23 @@ import vn.edu.nlu.fit.elearning.model.CourseTag;
 
 import java.util.List;
 
-public class CourseTagDao extends BaseDao implements BaseCrudDao<CourseTag, Integer> {
+public class CourseTagDao extends BaseDao {
 
-    @Override
-    public int create(CourseTag entity) {
+    public int create(int courseId, int tagId) {
         // TODO: Implement create logic
-        return 0;
+        return getJdbi().withHandle(handle -> {
+            return handle.createUpdate("INSERT INTO course_tags ( course_id, tag_id) VALUES (:courseId, :tagId)")
+                    .bind("courseId", courseId)
+                    .bind("tagId", tagId)
+                    .execute();
+        });
     }
 
-    @Override
     public CourseTag findById(Integer id) {
         // TODO: Implement findById logic
         return null;
     }
 
-    @Override
     public List<CourseTag> findAll() {
         return getJdbi().withHandle(handle -> {
             return handle.createQuery("SELECT ct.id AS course_tag_id, ct.course_id, c.title AS course_title, c.subtitle, c.price, ct.tag_id, t.name AS tag_name, t.slug AS tag_slug, t.created_at AS tag_created_at\n" +
@@ -29,23 +31,25 @@ public class CourseTagDao extends BaseDao implements BaseCrudDao<CourseTag, Inte
         });
     }
 
-    @Override
     public int update(CourseTag entity) {
         // TODO: Implement update logic
         return 0;
     }
 
-    @Override
     public int delete(Integer id) {
         // TODO: Implement delete logic
-        return 0;
+        return getJdbi().withHandle(handle -> {
+            return handle.createUpdate("DELETE FROM course_tags WHERE course_id = :courseId")
+                    .bind("courseId", id)
+                    .execute();
+        });
     }
 
     public List<Integer> findTagIdByCourseId(int courseId) {
         return getJdbi().withHandle(handle -> {
             return handle.createQuery("SELECT tag_id\n" +
-                    "FROM course_tags\n" +
-                    "WHERE course_id = :courseId\n")
+                            "FROM course_tags\n" +
+                            "WHERE course_id = :courseId\n")
                     .bind("courseId", courseId)
                     .mapTo(Integer.class)
                     .list();
--- src/main/java/vn/edu/nlu/fit/elearning/services/CourseTagService.java
+++ src/main/java/vn/edu/nlu/fit/elearning/services/CourseTagService.java
@@ -13,9 +13,16 @@ public class CourseTagService {
         this.ctd = new CourseTagDao();
     }
 
-    public int createCourseTag(CourseTag courseTag) {
-        // TODO: Implement creation logic
-        return 0;
+    public int createCourseTag(int courseId, String[] tagIds) {
+        if (tagIds == null || tagIds.length == 0) {
+            return 1; // không chọn tag vẫn OK
+        }
+
+        int total = 0;
+        for (String tagId : tagIds) {
+            total += this.ctd.create(courseId, Integer.parseInt(tagId));
+        }
+        return total;
     }
 
     public List<CourseTag> getAllCourseTags() {
@@ -32,8 +39,8 @@ public class CourseTagService {
         // TODO: Implement update logic
     }
 
-    public void deleteCourseTag(int id) {
-        // TODO: Implement delete logic
+    public int deleteCourseTag(int id) {
+        return ctd.delete(id);
     }
 
     public List<Integer> getAllTagIdByCourseId(int courseId) {
