c13fe41a392cc1489241ebf5c45777efe72caa17
Nguyen Ngoc Minh <23130192@st.hcmuaf.edu.vn>
2026-04-17 22:53:58 +0700

    fix(sign-up): validator and if success then status of user is active

--- src/main/java/vn/edu/nlu/fit/elearning/feature/auth/controller/SignUpController.java
+++ src/main/java/vn/edu/nlu/fit/elearning/feature/auth/controller/SignUpController.java
@@ -5,22 +5,27 @@ import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 import vn.edu.nlu.fit.elearning.common.container.BeanContainer;
 import vn.edu.nlu.fit.elearning.common.external.mail.MailService;
+import vn.edu.nlu.fit.elearning.common.helper.validator.sign_up.SignUpValidator;
 import vn.edu.nlu.fit.elearning.feature.access_token.model.AccessToken;
 import vn.edu.nlu.fit.elearning.feature.access_token.service.AccessTokenService;
+import vn.edu.nlu.fit.elearning.feature.auth.service.AuthService;
 import vn.edu.nlu.fit.elearning.feature.user.dto.response.UserShortResponse;
 import vn.edu.nlu.fit.elearning.feature.user.service.UserService;
 import java.io.IOException;
+import java.util.Map;
 
 @WebServlet(name = "SignUpController", value = "/sign-up")
 public class SignUpController extends HttpServlet {
     private transient UserService userService;
     private transient AccessTokenService accessTokenService;
+    private transient AuthService authService;
 
     @Override
     public void init() throws ServletException {
         super.init();
         this.userService = BeanContainer.getBean(UserService.class);
         this.accessTokenService = BeanContainer.getBean(AccessTokenService.class);
+        this.authService = BeanContainer.getBean(AuthService.class);
     }
 
     @Override
@@ -39,21 +44,14 @@ public class SignUpController extends HttpServlet {
             String password = request.getParameter("password");
             String confirmPassword = request.getParameter("confirmPassword");
 
-            if (email.isEmpty() || username.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()) {
-                throw new IllegalArgumentException("Vui lòng nhập đầy đủ thông tin!");
-            }
+            Map<String, String> errors = SignUpValidator.validate(
+                    email, username, password, confirmPassword, userService
+            );
 
-            if (!password.equals(confirmPassword)) {
-                throw new IllegalArgumentException("Mật khẩu và mật khẩu xác nhận không khớp!");
-            }
-
-            UserShortResponse user = null;
-            if ((user = userService.getUserByEmail(email)) != null) {
-                throw new IllegalArgumentException("Email đã tồn tại!");
-            }
-
-            if (userService.existsUserByUsername(username)) {
-                throw new IllegalArgumentException("Tên người dùng đã tồn tại !");
+            if (!errors.isEmpty()) {
+                request.setAttribute("errors", errors);
+                request.getRequestDispatcher("/views/pages/auth/sign-up.jsp").forward(request, response);
+                return;
             }
 
             // Tạo token xác thực
