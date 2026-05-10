8eaf70bda112063def687a1a0a30fd836285526e
Nguyen Ngoc Minh <23130192@st.hcmuaf.edu.vn>
2026-05-10 10:06:07 +0700

    feat(request): view table list request

---
+++ src/main/java/vn/edu/nlu/fit/elearning/feature/contact/controller/admin/RequestManagementController.java
@@ -0,0 +1,21 @@
+package vn.edu.nlu.fit.elearning.feature.user_request.controller.admin;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+import vn.edu.nlu.fit.elearning.common.base.BaseController;
+
+import java.io.IOException;
+
+@WebServlet(name = "RequestManagementController", value = "/RequestManagementController")
+public class RequestManagementController extends BaseController {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
